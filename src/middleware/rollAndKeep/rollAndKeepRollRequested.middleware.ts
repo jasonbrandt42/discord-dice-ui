4ef0a92c6363a6a4fc304c806115c2d18a76222f
Przemek <pb@spam.pl>
2021-01-20 11:59:20 +0100

    Send Discord msg

--- src/middleware/rollAndKeep/rollAndKeepRollRequested.middleware.ts
+++ src/middleware/rollAndKeep/rollAndKeepRollRequested.middleware.ts
@@ -1,6 +1,9 @@
+import { requestMsgReady } from '../../actions/roll.actions';
 import { ROLL_AND_KEEP_ROLL_REQUESTED, rollAndKeepDiceRolled } from '../../actions/rollAndKeep.actions';
 import { D10 } from '../../consts/diceConstants';
+import { getColor } from '../../utils/getColor';
 import getResultsArray from '../../utils/getResultsArray';
+import joinAsBlocks from '../../utils/joinAsBlocks';
 
 export default (store: any) => (next: any) => (action: any) => {
 	if (action.type === ROLL_AND_KEEP_ROLL_REQUESTED) {
@@ -16,9 +19,46 @@ export default (store: any) => (next: any) => (action: any) => {
 			return results;
 		};
 
-		const results = new Array(diceAmount).fill('_').map(() => getResults());
+		const results: Array<Array<number>> = new Array(diceAmount)
+			.fill('_')
+			.map(() => getResults());
 
 		if (results.length) {
+			const state = store.getState();
+			const { userSettings, rerollCount } = state;
+			const username = userSettings.username || 'USERNAME_MISSING';
+
+			const description = results.reduce((acc: string, resultsRow: number[]) => {
+				const sum = resultsRow.reduce((a, b) => (a + b), 0);
+				return acc + `${sum} (${joinAsBlocks(resultsRow, '+ ', true)})\n`
+			}, '');
+
+			const fields = [];
+
+			if (modifier) {
+				const modifierIcon = modifier > 0 ? ':arrow_up:' : ':arrow_down:';
+				const modifierSign = modifier > 0 ? '+' : '';
+				fields.push({
+					name: `${modifierIcon} Modifier:`,
+					value: `\`${modifierSign}${modifier}\``
+				})
+			}
+
+			if (rerollCount) {
+				const rerollCountStr = rerollCount === 1 ? 'time' : 'times';
+				fields.push({
+					name: `:game_die: Reroll Count:`,
+					value: `The dice have been rerolled \`${rerollCount}\` ${rerollCountStr}.`
+				})
+			}
+
+			store.dispatch(requestMsgReady({
+				msgTitle: `${username}'s roll results:`,
+				description,
+				color: getColor(),
+				fields
+			}));
+			
 			store.dispatch(rollAndKeepDiceRolled({
 				results,
 				modifier: Number(modifier)
